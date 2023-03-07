7ced2cc3cae9f3a9d4ba8c834056c9f76a458e1b
vladistrategen <vlad.istrate02@gmail.com>
2023-03-07 09:05:29 +0200

    created asset loader to handle textures, added more logic for sprites and textures.

--- ChickenInvadersClone/GameObject.cpp
+++ ChickenInvadersClone/GameObject.cpp
@@ -1,13 +1,65 @@
 #include "GameObject.h"
+#include <iostream>
+
 
 GameObject::GameObject()
 {
 	this->health = 50;
 	this->damage = 10;
 	this->position = sf::Vector2f(0, 0);
-	this->velocity = sf::Vector2f(0, 0);
-	this->acceleration = sf::Vector2f(0, 0);
-	this->size = sf::Vector2f(0, 0);
+	this->velocity = 5;
+	this->acceleration = 1;
+	this->size = 1;
 
-	
 }
+
+
+void GameObject::UpdatePosition(float x_offset, float y_offset)
+{
+	this->position.x += x_offset;
+	this->position.y += y_offset;
+}
+
+sf::Vector2f GameObject::getPosition()
+{
+	return this->position;
+}
+
+void GameObject::setPosition(sf::Vector2f position)
+{
+	this->position = position;
+}
+
+void GameObject::setTexture(sf::Texture& texture)
+{
+	this->texture = texture;
+}
+
+void GameObject::loadTexture()
+{
+	if (!this->texture.loadFromFile(this->pathToTexture))
+	{
+        // Handle error
+		std::cerr << "[!] Cannot load file: \"" << this->pathToTexture << "\". Exiting...\n";
+		std::cin.get();
+		exit(1);
+	}
+}
+
+void GameObject::setSprite(sf::Sprite& sprite)
+{
+	this->sprite = sprite;
+}
+
+void GameObject::loadSprite()
+{
+	this->sprite.setTexture(this->texture);
+	this->sprite.setPosition(this->position);
+}
+
+sf::Sprite* GameObject::getSpritePointer()
+{
+	return &this->sprite;
+}
+
+
--- ChickenInvadersClone/GameObject.h
+++ ChickenInvadersClone/GameObject.h
@@ -1,28 +1,39 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-class GameObject
+#include <string>
+using std::string;
+class GameObject: public sf::Drawable
 {
 public:
 	GameObject();
-	~GameObject();
-	void Draw();
+	~GameObject() {}
 	void UpdatePosition(float x_offset, float y_offset);
 	sf::Vector2f getPosition();
 	void setPosition(sf::Vector2f position);
-	void setTexture(sf::Texture texture);
-	void setSprite(sf::Sprite sprite);
+	void setTexture(sf::Texture& texture);
+	void loadTexture();
+	void setSprite(sf::Sprite& sprite);
+	void loadSprite();
 	sf::Sprite* getSpritePointer();
-	int getHealth();
-	void setHealth(int health);
-	int getDamage();
-	void setDamage(int damage);
-	float getVelocity();
-	void setVelocity(float velocity);
-	float getAcceleration();
-	void setAcceleration(float acceleration);
-	int getSize();
-	void setSize(int size);
+
+	int getHealth(){ return this->health; }
+	void setHealth(int health) { this->health = health; }
+
+	int getDamage() { return this->damage; }
+	void setDamage(int damage) { this->damage = damage; }
 	
+	float getVelocity() { return this->velocity; }
+	void setVelocity(float velocity) { this->velocity = velocity; }
+	
+	float getAcceleration() { return this->acceleration; }
+	void setAcceleration(float acceleration) { this->acceleration = acceleration; }
+
+	int getSize() { return this->size; }
+	void setSize(int size) { this->size = size; }
+	
+	string getPathToTexture() { return this->pathToTexture; }
+	void setPathToTexture(string pathToTexture) { this->pathToTexture= pathToTexture; }
+
 
 
 private:
@@ -34,6 +45,10 @@ private:
 	int size;
 	sf::Texture texture;
 	sf::Sprite sprite;
+
+	string pathToTexture;
+
+
 
 };
 
